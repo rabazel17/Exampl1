d22c2daf70b7b21ecf160480423de88d7551a408
Natasha <info@rabzel17.ru>
2022-11-30 13:22:46 +0300

    Text

--- Exsampele001_HelloConsole/Exampel004/Program.cs
+++ Exsampele001_HelloConsole/Exampel004/Program.cs
@@ -1,2 +1,8 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int namberA = 10;
+int namberB = 5;
+Console.WriteLine(namberA / namberB);
+
+double namberC = 12;
+double namberD = 5;
+Console.WriteLine(namberC/ namberD);
+
