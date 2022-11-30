f17441cab2ce6ce03644a22a608be520e3601e8a
Natasha <info@rabzel17.ru>
2022-11-30 13:56:54 +0300

    Text

---
+++ Exsampele001_HelloConsole/Example005/Program.cs
@@ -0,0 +1,6 @@
+﻿int namberA = new Random().Next (1,10);  //1 2 3 4...9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
+int result = namberA + namberB;
+Console.WriteLine(result);
